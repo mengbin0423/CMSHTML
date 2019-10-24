60b8c30c9812a381c70427399a889102967ccf38
mohen <apple@appledeMacBook-Pro.local>
2019-10-24 15:47:57 +0800

    vue搭建前端页面

---
+++ src/components/register.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="contain">
+    <div class="login">
+      <el-form
+          :model="ruleForm2"
+          status-icon
+          :rules="rules2"
+          ref="ruleForm2"
+          label-width="0"
+          class="demo-ruleForm"
+        >
+        <el-form-item prop="email">
+            <el-input v-model="ruleForm2.email" auto-complete="off" placeholder="请输入邮箱"></el-input>
+          </el-form-item>
+          <el-form-item prop="tel">
+            <el-input v-model="ruleForm2.tel" auto-complete="off" placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item prop="name">
+            <el-input v-model="ruleForm2.name" auto-complete="off" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="pass">
+            <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="输入密码"></el-input>
+          </el-form-item>
+          <el-form-item prop="checkPass">
+            <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="确认密码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm2')" style="width:100%;">注册并登录</el-button>
+            <p class="tologin" @click="gotoLogin">已有账号？立即登录</p>
+          </el-form-item>
+        </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    // <!--验证邮箱-->
+    let checkEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入邮箱'))
+      } else if (!this.checkEmail(value)) {
+        callback(new Error('邮箱不合法'))
+      } else {
+        callback()
+      }
+    }
+    // <!--校验用户名-->
+    let checkName = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'))
+      } else {
+        callback()
+      }
+    }
+    // <!--验证手机号是否合法-->
+    let checkTel = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入手机号码'))
+      } else if (!this.checkMobile(value)) {
+        callback(new Error('手机号码不合法'))
+      } else {
+        callback()
+      }
+    }
+    // <!--验证密码-->
+    let validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm2.checkPass !== '') {
+          this.$refs.ruleForm2.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    // <!--二次验证密码-->
+    let validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm2: {
+        email: '',
+        name: '',
+        pass: '',
+        checkPass: '',
+        tel: ''
+      },
+      rules2: {
+        pass: [{ validator: validatePass, trigger: 'change' }],
+        checkPass: [{ validator: validatePass2, trigger: 'change' }],
+        tel: [{ validator: checkTel, trigger: 'change' }],
+        email: [{ validator: checkEmail, trigger: 'change' }],
+        name: [{ validator: checkName, trigger: 'change' }]
+      },
+      buttonText: '发送验证码',
+      isDisabled: false, // 是否禁止点击发送验证码按钮
+      flag: true
+    }
+  },
+  methods: {
+    // <!--发送验证码-->
+
+    // <!--提交注册-->
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$axios.post('http://localhost:8080/register', {
+            email: this.ruleForm2.email,
+            phone: this.ruleForm2.tel,
+            name: this.ruleForm2.name,
+            password: this.ruleForm2.pass,
+            confirm: this.ruleForm2.checkPass
+          })
+            .then(function (response) {
+              console.log(response)
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    // <!--进入登录页-->
+    gotoLogin () {
+      this.$router.push({
+        path: '/login'
+      })
+    },
+    // 验证邮箱
+    checkEmail (str) {
+      let ref = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+      if (ref.test(str)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    // 验证手机号
+    checkMobile (str) {
+      let re = /^1\d{10}$/
+      if (re.test(str)) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .contain{
+    position: absolute;
+    width:100%;
+    height:100%;
+    background: #607D8B;
+  }
+   .contain .login{
+    width:285px;
+    position: relative;
+    left:50%;
+    top:100px;
+    border-radius: 10px;
+    background: #FFFFFF;
+    padding: 20px;
+    overflow: hidden;
+  }
+  .loading-wrapper {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: #aedff8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .register-wrapper img {
+    position: absolute;
+    z-index: 1;
+  }
+  .register-wrapper {
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+  #register {
+    max-width: 340px;
+    margin: 60px auto;
+    background: #fff;
+    padding: 20px 40px;
+    border-radius: 10px;
+    position: relative;
+    z-index: 9;
+  }
+.title {
+  font-size: 26px;
+  line-height: 50px;
+  font-weight: bold;
+  margin: 10px;
+  text-align: center;
+}
+.el-form-item {
+  text-align: center;
+}
+.login {
+  margin-top: 10px;
+  font-size: 14px;
+  line-height: 22px;
+  color: #1ab2ff;
+  cursor: pointer;
+  text-align: left;
+  text-indent: 8px;
+  width: 160px;
+}
+.login:hover {
+  color: #2c2fd6;
+}
+.code .el-form-item__content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.code button {
+  margin-left: 20px;
+  width: 140px;
+  text-align: center;
+}
+.el-button--primary:focus {
+  background: #409EFF;
+  border-color: #409EFF;
+  color: #fff;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/register'
+import login from '@/components/login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    }
+  ]
+})
